c352b526db91938f02cf587b01a9d20fd42a26a7
Kravmaga 01 <92754288+Kravmaga01@users.noreply.github.com>
2023-03-03 23:53:35 -0500

    add: chart,js

--- src/app/home/pages/store/pages/inventory/inventory.component.ts
+++ src/app/home/pages/store/pages/inventory/inventory.component.ts
@@ -1,27 +1,35 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-inventory',
-  templateUrl: './inventory.component.html',
-  styleUrls: ['./inventory.component.scss']
+selector: 'app-inventory',
+templateUrl: './inventory.component.html',
+styleUrls: ['./inventory.component.scss']
 })
 export class InventoryComponent {
 
-    data = [
-      { id: 1, first: 'Mark', last: 'Otto', handle: '@mdo' },
-      { id: 2, first: 'Jacob', last: 'Thornton', handle: '@fat' },
-      { id: 3, first: '', last: 'Larry the Bird', handle: '@twitter' }
-    ];
-  
-    isEditing = false;
-  
-    editTable() {
-      this.isEditing = true;
-    }
-  
-    saveTable() {
-      this.isEditing = false;
-      // Aquí puedes agregar la lógica para guardar los datos en algún servicio o API.
-    }
-  
+
+data = [
+  { id: 1, first: 'Mark', last: 'Otto', handle: '@mdo' },
+  { id: 2, first: 'Jacob', last: 'Thornton', handle: '@fat' },
+  { id: 3, first: '', last: 'Larry the Bird', handle: '@twitter' }
+];
+
+isEditing = false;
+
+editTable() {
+  this.isEditing = true;
+}
+
+saveTable() {
+  this.isEditing = false;
+  // Aquí puedes agregar la lógica para guardar los datos en algún servicio o API.
+}
+
+addRow() {
+  this.data.push({ id: this.data.length + 1, first: '', last: '', handle: '' });
+}
+
+deleteRow(index: number) {
+  this.data.splice(index, 1);
+}
 }
